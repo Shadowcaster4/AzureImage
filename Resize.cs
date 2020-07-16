2873afbe36dabf75670aeca0fda4503b91960a5c
Teofil Markowy <any@any.any>
2020-07-16 19:33:57 +0200

    resizer update

--- Resize.cs
+++ Resize.cs
@@ -16,30 +16,40 @@ using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.Fonts;
 using Azure.Storage.Blobs.Models;
 using System.Web.Http;
-
+using System.Net.Http;
+using System.Net;
+using System.Reflection.Metadata;
 
 namespace ImageResizer
 {
 
     public static class Resize
     {
-        private static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+        //connection string
+        private static  readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
 
+        //method for creating resized images
         private static void CreateImage(MemoryStream imageFromStorage, BlobContainerClient containerClient, string blobFullName, int width, int height,int padding, string text = "duzo duzo duzo")
         {
+
+            //font & color variables
             var fo = SystemFonts.Find("Arial");
             var font = new Font(fo, 20, FontStyle.Regular);
             PointF myPointIs = new PointF((width - (12 * text.Length)), height - 20);
             Color Rgba = Color.FromRgba(0, 155, 0, 130);
 
+
             using (var output = new MemoryStream())
             {
+
                     Image<Rgba32> image = (Image<Rgba32>)Image.Load(imageFromStorage.ToArray());
-                                                        
+                     
+                    //resize & watermark
                     image.Mutate(x => x
                         .Resize(width, height).DrawText(text, font, Rgba, myPointIs)
                     );
-
+                    
+                    //padding 
                     if(padding>0)
                     {
                         var tmpImage = new Image<Rgba32>(width + padding, height + padding, Color.FromRgb(0, 0, 0));
@@ -47,11 +57,13 @@ namespace ImageResizer
                         image = tmpImage;
                     }
                     
+                    //saving image with correct encoder to stream
                     if (blobFullName.Substring(blobFullName.Length - 3) == "jpg")
                         image.Save(output, new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder());
                     if (blobFullName.Substring(blobFullName.Length - 3) == "png")
                         image.Save(output, new SixLabors.ImageSharp.Formats.Png.PngEncoder());
 
+                    //saving image to blobstorage
                     output.Position = 0;
                     if (padding > 0) 
                     {
@@ -66,7 +78,7 @@ namespace ImageResizer
                                
             }
         }
-
+        //checks if query parameters are correct
         private static string CheckQuery(HttpRequest request, string fileName)
         {
             string error = "";
@@ -80,33 +92,43 @@ namespace ImageResizer
 
 
         [FunctionName("Resize")]
-        public static async Task<IActionResult> Run(
+        public static async Task<HttpResponseMessage> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Resize/{name}")] HttpRequest req,
             string name,
             ILogger log)
         {
+            var resp = new HttpResponseMessage();
             try
             {
-
+                //returns BadRequestMessage if query parameters are incorrect
                 if (!string.IsNullOrEmpty(CheckQuery(req, name)))
                 {
-                    return new BadRequestErrorMessageResult(CheckQuery(req, name));
+                    resp.StatusCode = HttpStatusCode.BadRequest;
+                    resp.Content = new StringContent(CheckQuery(req, name));
+                    return resp;
                 }
 
                 int padding = string.IsNullOrEmpty(req.Query["padding"]) ? 0 : Int32.Parse(req.Query["padding"]);
                 int width = Int32.Parse(req.Query["width"]);
                 int height = Int32.Parse(req.Query["height"]);
 
+
+                //returns BadREquestMessage if width/height/padding are out of range
                 if (width > 2000 || width < 10 || height > 2000 || height < 10 || padding < 0 || padding > width / 2 + height / 2 ? true : false)
                 {
-                    return new BadRequestErrorMessageResult("wrong parameter values");
+                    resp.StatusCode = HttpStatusCode.BadRequest;
+                    resp.Content = new StringContent("wrong parameter values");
+                    return resp;                    
                 }
 
+                //blob service connection
                 var thumbContainerName = name.Remove(name.Length - 4);
                 var blobServiceClient = new BlobServiceClient(BLOB_STORAGE_CONNECTION_STRING);
 
+                //checks if image exist in storage
                 if (blobServiceClient.GetBlobContainerClient(thumbContainerName).Exists())
                 {
+                    //get image container
                     var blobContainterClient = blobServiceClient.GetBlobContainerClient(thumbContainerName);
 
                     string path = "";
@@ -118,16 +140,17 @@ namespace ImageResizer
                     {
                         path = width + "/" + height + "/"  + name;
                     }
-                        
-
-
+                     
+                    //get image
                     var blobObj = blobContainterClient.GetBlobBaseClient(path);
 
+                    //if requested image already exists then return image uri else create new reqested image
                     if (blobObj.Exists())
                     {
-                        log.LogInformation("exists");
-                        return new OkObjectResult(blobObj.Uri);
-
+                        resp.StatusCode = HttpStatusCode.OK;
+                        resp.Content = new StringContent(blobObj.Uri.ToString());
+                        return resp;
+                    
                     }
                     else if (blobContainterClient.GetBlobBaseClient(name).Exists())
                     {
@@ -140,17 +163,25 @@ namespace ImageResizer
                 }
                 else
                 {
-                    return new BadRequestErrorMessageResult("requested picture doesn't exists");
+                    resp.StatusCode = HttpStatusCode.BadRequest;
+                    resp.Content = new StringContent("requested picture doesn't exists");
+                    return resp;
+                   
                 }
             }
             catch (Exception e)
             {
                 log.LogInformation(e.Message);
-                return new BadRequestErrorMessageResult("Something went wrong");
+                resp.StatusCode = HttpStatusCode.BadRequest;
+                resp.Content = new StringContent("Something went wrong" + e.Message);
+                return resp;
+               
             }
 
-           
-            return new OkObjectResult("Image resized succesfully");
+            resp.StatusCode = HttpStatusCode.OK;
+            resp.Content = new StringContent("Image resized succesfully");
+            return resp;
+            
             
         }
     }
